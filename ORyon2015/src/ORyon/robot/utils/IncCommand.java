65ca278612acf20be54bb0e7a8bde3b1aff79a6b
Sam Dietrich <samdietrich@hotmail.com>
2015-01-03 13:05:04 -0600

    Added common files

---
+++ ORyon2015/src/ORyon/robot/utils/IncCommand.java
@@ -0,0 +1,55 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ORyon.robot.utils;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Command;
+
+/**
+ *
+ * @author Sam
+ * 
+ * Can decrement if increment is negative
+ */
+public class IncCommand extends Command {
+    private double inc_ = 0.0;
+    private SpeedController motor_;
+    
+    private IncCommand() {
+    }
+    
+    public IncCommand(SpeedController cont){
+        this(cont, 0.1);
+    }
+    
+    public IncCommand(SpeedController cont, double increment){
+        motor_ = cont;
+        inc_ = increment;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        motor_.set(motor_.get() + inc_);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
---
+++ ORyon2015/src/ORyon/robot/utils/SetCommand.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ORyon.robot.utils;
+
+import edu.wpi.first.wpilibj.SpeedController;
+import edu.wpi.first.wpilibj.command.Command;
+/**
+ *
+ * @author Sam
+ * 
+ * Can decrement if increment is negative
+ */
+public class SetCommand extends Command {
+    private double setpoint_ = 0.0;
+    private SpeedController motor_;
+    
+    private SetCommand() {
+    }
+    
+    public SetCommand(SpeedController cont){
+        this(cont, 0.1);
+    }
+    
+    public SetCommand(SpeedController cont, double setpoint){
+        motor_ = cont;
+        setpoint_ = setpoint;
+    }
+
+    // Called just before this Command runs the first time
+    protected void initialize() {
+        motor_.set(setpoint_);
+    }
+
+    // Called repeatedly when this Command is scheduled to run
+    protected void execute() {
+    }
+
+    // Make this return true when this Command no longer needs to run execute()
+    protected boolean isFinished() {
+        return true;
+    }
+
+    // Called once after isFinished returns true
+    protected void end() {
+    }
+
+    // Called when another command which requires one or more of the same
+    // subsystems is scheduled to run
+    protected void interrupted() {
+    }
+}
